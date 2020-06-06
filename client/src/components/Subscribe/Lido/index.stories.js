4b28910c5a48d40f3f0c5f683822998c0fb14ac0
defending1 <work.albertodefendi@gmail.com>
2020-06-06 16:21:59 +0200

    :new: Storybook

---
+++ client/src/components/Subscribe/Lido/index.stories.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Container, Input, Text, Button } from '../style'
+import { useForm } from "react-hook-form";
+export default { title: "form/Lido" };
+
+
+export const Lido = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = values => console.log(values);
+  return (
+    <Container onSubmit={handleSubmit(onSubmit)}>
+    <h1>Lido</h1>
+      <Input
+      ref={register}
+        name="Nome"
+        placeholder="Nome"
+      />
+      <Text
+      rows="5"
+      cols="80"
+        name="Descrizione"
+ref={register}
+        placeholder="Descrizione"
+      />
+
+      <Input
+      ref={register}
+        name="Capienza"
+        type="number"
+        placeholder="Capienza"
+      />
+      <Button type="submit">Submit</Button>
+    </Container>
+  );
+};
---
+++ client/src/components/Subscribe/style.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Container = styled.form`
+  display: flex;
+  flex-flow: column;
+  align-items: stretch;
+`
+
+export const Input = styled.input`
+  font-size: 2em;
+  background: red;
+  border: none;
+  margin: 0.5em 0;  
+  border-radius: 0.5em;
+  padding: 0.5em;
+`
+
+export const Button = styled.button`
+  font-size: 2em;
+  border-radius: 2em;
+  border: none;
+  padding: 0.5em;
+
+`
+export const Text = styled.textarea`
+  font-size: 2em;
+  background: red;
+  border: none;
+  margin: 0.5em 0;  
+  border-radius: 0.5em;
+  padding: 0.5em;
+`
